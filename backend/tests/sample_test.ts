e48d919232c099eae7807c5311208d922181008c
G suman <sumangarimiddi@gmail.com>
2026-06-06 14:41:36 +0530

    #2 | adds sample test for Ci set-up | suman/pradeep

---
+++ backend/tests/sample_test.ts
@@ -0,0 +1,8 @@
+import { describe, it } from "@std/testing";
+import {  assertEquals } from "@std/assert";
+
+describe("Sample Test", () => {
+  it("should pass", () => {
+    assertEquals(303, 303);
+  });
+});
