0a0d735de5992dfe0004f1219e2c4cb2b1e06805
G suman <sumangarimiddi@gmail.com>
2026-06-06 15:55:19 +0530

    adds version in deno.json

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "../generated/prisma/index.js";
-import { PrismaPg } from "npm:@prisma/adapter-pg";
-import pg from "npm:pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import pg from "pg";
 
 // 1. Initialize the PostgreSQL driver connection pool
 const pool = new pg.Pool({
