74a017639afa41d9ece1f5e135639b17e7bf6fcc
bhagya <snehakarada26@gmail.com>
2026-06-11 11:13:40 +0530

    #10 | add. api test for delete question feature | Bhagya, Bhanu

---
+++ backend/tests/apiTests/question_test.ts
@@ -0,0 +1,23 @@
+import { assertEquals } from "@std/assert";
+import { describe, it } from "@std/testing";
+import { createApp } from "../../src/app.ts";
+import { getHandlers } from "../../main.ts";
+
+describe("deleteQuestion", () => {
+  it("return 404 when the question does not exist with the id", async () => {
+
+    const app = createApp(getHandlers());
+    const response = await app.request("/api/questions/1", { method: "DELETE" });
+
+    assertEquals(404, response.status)
+  });
+
+  it("return 400 when the give id is not a number", async () => {
+
+    const app = createApp(getHandlers())
+    const response = await app.request("/api/questions/a", { method: "DELETE" });
+
+
+    assertEquals(400, response.status)
+  })
+});
