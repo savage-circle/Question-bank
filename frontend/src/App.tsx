598726abc7babc6b9df60f2c81c01aed0059db1b
varrapavani <pavani21057cm051@gmail.com>
2026-07-02 12:40:56 +0530

    Pavani | Added basic structure for react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div>
+            <h1>Hello, question-bank-frontend 👋</h1>
+            <p>Your React app is running.</p>
+        </div>
+    )
+}
+
+export default App;
