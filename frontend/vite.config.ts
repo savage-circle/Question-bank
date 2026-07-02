4fa9f4e631bbd8d8b07c6d64c60ffc8b602abbdd
varrapavani <pavani21057cm051@gmail.com>
2026-07-02 19:45:47 +0530

    Pavani | Fixed prettier

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   test: {
     globals: true,
     environment: 'jsdom',
