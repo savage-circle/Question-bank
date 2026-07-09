0d28f98863773ad1d9a04d01c3637b2d4d2c9d93
malleswaribatta <battamalleswari29@gmail.com>
2026-07-09 11:08:34 +0530

    #30 | Created base layout | Malleswari

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,72 @@
+import { Box } from '@mui/system';
+
 function App() {
   return (
     <div>
-      <h1>Hello, question-bank-frontend 👋</h1>
-      <p>Your React app is running.</p>
+      <Box
+        sx={{
+          width: '100vw',
+          height: '100vh',
+          border: '1px solid #BDBDBD',
+          boxSizing: 'border-box',
+          display: 'flex',
+          flexDirection: 'column',
+          bgcolor: '#fff',
+        }}
+      >
+        {/* Header */}
+        <Box
+          sx={{
+            height: 70,
+            borderBottom: '1px solid #BDBDBD',
+          }}
+        ></Box>
+
+        {/* Toolbar */}
+        <Box
+          sx={{
+            height: 70,
+            borderBottom: '1px solid #BDBDBD',
+          }}
+        >
+          {' '}
+        </Box>
+
+        {/* Body */}
+        <Box
+          sx={{
+            flex: 1,
+            px: 3,
+            py: 4,
+            display: 'flex',
+            gap: 5,
+          }}
+        >
+          {/* Sidebar */}
+          <Box
+            sx={{
+              width: 280,
+              border: '1px solid #BDBDBD',
+            }}
+          />
+
+          {/* Main Content */}
+          <Box
+            sx={{
+              flex: 1,
+              border: '1px solid #BDBDBD',
+            }}
+          />
+        </Box>
+
+        {/* Footer */}
+        <Box
+          sx={{
+            height: 80,
+            borderTop: '1px solid #BDBDBD',
+          }}
+        />
+      </Box>
     </div>
   );
 }
